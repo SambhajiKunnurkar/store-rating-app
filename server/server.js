9534f9843543aa14d77f56905387754490babb6b
Sambhaji Kunnurkar <sambhajikunnurkar2002@gmail.com>
2025-08-20 15:35:57 +0530

    Backend Setup

---
+++ server/server.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+
+
+dotenv.config();
+
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.use(cors());
+
+
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
